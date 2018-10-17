6e5e132a6049112410e2dee524863c514dfa620d
Jongho Park <jh0x70@gmail.com>
2018-10-17 10:46:31 +0900

    Footer 추가

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "src/typed-components";
+import { Icon, Text } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
+const Container = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 200px;
+  background-color: grey;
+  color: white;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const Box = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 30px;
+`;
+
+const CustomIcon = styled(Icon)`
+  margin-right: 10px;
+`;
+
+class Footer extends React.Component {
+  public render() {
+    return (
+      <Container>
+        <Box>
+          <div>
+            <Text>
+              <CustomIcon icon={IconNames.MAP_MARKER} />
+              adress
+            </Text>
+          </div>
+          <div>
+            <Text>
+              <CustomIcon icon={IconNames.PHONE} />
+              tel
+            </Text>
+          </div>
+          <div>
+            <Text>
+              <CustomIcon icon={IconNames.DOCUMENT} />
+              fax
+            </Text>
+          </div>
+          <div>
+            <Text>
+              <CustomIcon icon={IconNames.ENVELOPE} />
+              email
+            </Text>
+          </div>
+        </Box>
+        <Box>
+          <div>about company</div>
+          <div>other links</div>
+        </Box>
+      </Container>
+    );
+  }
+}
+
+export default Footer;
